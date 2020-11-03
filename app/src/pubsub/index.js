11fd939bfc516e84b08140b615830ab1b7fcaca6
Brad Campbell <brad.campbell@cloudreach.com>
2020-11-03 10:08:21 -0500

    [feat] add SNS for notifications

---
+++ app/src/pubsub/index.js
@@ -0,0 +1,1 @@
+if (process.env.SNS_TOPIC_ARN_DEFAULT) module.exports = require('./sns');
---
+++ app/src/pubsub/sns.js
@@ -0,0 +1,30 @@
+var AWS = require('aws-sdk');
+const sns = new AWS.SNS({
+    apiVersion: '2010-03-31',
+    region: process.env.AWS_REGION || 'us-east-1'
+});
+
+const actionMappings = {
+    update: 'Item was updated',
+    add: 'Item was added',
+    delete: 'Item was deleted',
+    complete: 'Item was marked complete',
+    incomplete: 'Item was marked incomplete',
+};
+
+async function publish(topicArn, op, itemName) {
+    return new Promise((acc, rej) => {
+        var params = {
+          Message: `${actionMappings[op]}: ${itemName}`,
+          TopicArn: topicArn
+        };
+        sns.publish(params, function(err, data) {
+            if (err) return rej(err);
+            acc();
+        });
+    });
+}
+
+module.exports = {
+    publish,
+};
