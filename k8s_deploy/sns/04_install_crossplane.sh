c3ff687a59c8107b706f8a0276195075cfde6de0
Brad Campbell <brad.campbell@cloudreach.com>
2020-11-12 18:32:49 -0500

    [fix] parametrize profile; update crossplane version

--- k8s_deploy/sns/04_install_crossplane.sh
+++ k8s_deploy/sns/04_install_crossplane.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-PROFILE="default"
+PROFILE="${1:-default}"
+VSN="0.14.0"
+VSN_TRIM=${VSN%".0"}
 
 set -euxo pipefail
 
@@ -10,17 +12,19 @@ helm repo add crossplane-alpha https://charts.crossplane.io/alpha
 helm repo update
 helm install crossplane \
 	--namespace crossplane-system crossplane-alpha/crossplane \
-	--set alpha.oam.enabled=true
+	--set alpha.oam.enabled=true --version ${VSN}
 
 # Congfigure kubectl for crossplane usage
-curl -sL https://raw.githubusercontent.com/crossplane/crossplane/release-0.13/install.sh | sh
+VERSION=${VSN} CHANNEL="alpha" curl -sL  https://raw.githubusercontent.com/crossplane/crossplane/release-0.14/install.sh | sh
 sudo mv kubectl-crossplane /usr/local/bin
 sudo chmod +x /usr/local/bin/kubectl-crossplane
 kubectl plugin list | grep crossplane
-kubectl crossplane install provider crossplane/provider-aws:v0.12.0
+kubectl crossplane install provider crossplane/provider-aws:alpha
 
 # Set up AWS credentials
-AWS_PROFILE=${PROFILE} && echo -e "[default]\naws_access_key_id = $(aws configure get aws_access_key_id --profile $AWS_PROFILE)\naws_secret_access_key = $(aws configure get aws_secret_access_key --profile $AWS_PROFILE)" > creds.conf
+echo -e "[default]\naws_access_key_id = $(aws configure get aws_access_key_id --profile $PROFILE)\naws_secret_access_key = $(aws configure get aws_secret_access_key --profile $PROFILE)" > creds.conf
 kubectl create secret generic aws-creds -n crossplane-system --from-file=key=./creds.conf
 rm ./creds.conf
-kubectl apply -f https://raw.githubusercontent.com/crossplane/crossplane/master/docs/snippets/configure/aws/providerconfig.yaml
+# provider config doesn't seem to work without the sleep
+sleep 30
+kubectl apply -f https://raw.githubusercontent.com/crossplane/crossplane/release-${VSN_TRIM}/docs/snippets/configure/aws/providerconfig.yaml
